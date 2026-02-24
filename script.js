0e6e50c8089bf1e8b9e28ccc56617cbd0e42f31e
Dev Gupta <devgupta0320@gmail.com>
2026-02-24 09:54:29 +0530

    variable

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 console.log("Hello Console");
-document.write("Hello Browser!");
+//document.write("Hello Browser!");
+
+num=10;
+console.log(num);
+console.log(typeof(num));
+num="Dev Gupta";
+console.log(num);
+console.log(typeof(num));
+num=true;
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("True,Symbols are same" );
+}
+else{
+    console.log("False,Symbols are not same");
+}
