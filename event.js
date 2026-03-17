8f20f330c332e2f2bb6a5327feb7c163fe8f2877
Dev Gupta <devgupta0320@gmail.com>
2026-03-17 10:00:09 +0530

    events

---
+++ event.js
@@ -0,0 +1,15 @@
+function onClick(){
+    console.log("clicked");
+}
+function onmouseOver(){
+    console.log("mouse_over");
+}
+function onmouseout(){
+    console.log("mouse_out");
+}
+function onmouseUp(){
+    console.log("mouse_up");
+}
+function onmouseDown(){
+    console.log("mouse_down");
+}
